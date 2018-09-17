4e4cb243d193e9712844ff78a6129d2238951e14
Jin Nam <developsick@gmail.com>
2018-09-18 02:00:09 +0900

    add lec7

---
+++ lec7/app.js
@@ -0,0 +1,27 @@
+(function () {
+'use strict';
+
+angular.module('NameCalculator', [])
+
+.controller('NameCalculatorController', function ($scope) {
+
+    $scope.name = ""
+    $scope.totalValue = 0;
+
+    $scope.displayNumeric = function () {
+        var totalNameValue =
+        calculateNumericForString($scope.name); //get the total value
+        $scope.totalValue = totalNameValue;
+    };
+
+    function calculateNumericForString(string) {
+        var totalStringValue = 0;
+        for (var i = 0 ; i < string.length ; i++){
+            totalStringValue += string.charCodeAt(i);
+        }
+
+        return totalStringValue;
+    }
+});
+
+})();
