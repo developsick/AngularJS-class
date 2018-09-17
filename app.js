2f3450a154595588995f5496af574bf9daee4594
umd <gin.nam@sk.com>
2018-09-17 18:01:57 +0900

    add examples using ng-model

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ angular.module('myFirstApp', [])
 
 .controller('MyFirstController', function ($scope) {
     $scope.name = "devsick"
+    $scope.sayHello = function () {
+        return "Hello Coursera!";
+    };
 });
 
 })();
