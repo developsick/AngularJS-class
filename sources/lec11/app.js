ff043f8024cb20666674dee2fdbcad6145e14beb
umd <gin.nam@sk.com>
2018-09-27 18:07:28 +0900

    add lec11 files (w/ some images)

--- sources/lec11/app.js
+++ sources/lec11/app.js
@@ -7,9 +7,13 @@ angular.module('MsgApp', [])
 MsgController.$inject = ['$scope'];
 function MsgController($scope) {
   $scope.name = "Jin";
-
+  $scope.stateOfBeing = "fire";
   $scope.sayMessege = function () {
     return "is saying messeges";
+  };
+
+  $scope.flip = function () {
+      $scope.stateOfBeing = "water";
   };
 }
 
